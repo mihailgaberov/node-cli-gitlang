141e5dc5789cef3a5badc89cd0f2bb77dcb96f0b
Mihail Gaberov <mihail.gaberov@gmail.com>
2017-05-01 15:23:54 +0200

    Finished the app - added functions for getting the language property from each repo and define which language is the most frequently used.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 /**
  * Created by Mihail on 4/30/2017.
  */
-'use strict';
+'use strict'
 
-var chalk = require('chalk');
-var clear = require('clear');
-var figlet = require('figlet');
-var CLI = require('clui');
-var inquirer = require('inquirer');
-var util = require('util');
-var Spinner = CLI.Spinner;
-var https = require('https');
-var request = require('request');
+var chalk = require('chalk')
+var clear = require('clear')
+var figlet = require('figlet')
+var CLI = require('clui')
+var inquirer = require('inquirer')
+var util = require('util')
+var Spinner = CLI.Spinner
+var https = require('https')
+var request = require('request')
 
-var URL_GIT_REPOS = 'https://api.github.com/users/%s/repos';
+var URL_GIT_REPOS = 'https://api.github.com/users/%s/repos'
 
-clear();
-console.log(chalk.yellow(figlet.textSync('Git Lang', {horizontalLayout: 'full'})));
+var arrLangs = []
 
-getInput(getRepos);
+clear()
+console.log(chalk.yellow(figlet.textSync('Git Lang', {horizontalLayout: 'full'})))
+
+getInput(getRepos)
 
 function getInput(callback) {
   var questions = [
@@ -28,40 +30,76 @@ function getInput(callback) {
       message: 'Enter a valid account name of a Github user:',
       validate: function (value) {
         if (typeof value === 'string' && value.length > 2 && value.match(/[^a-zA-Z0-9]/g) === null) {
-          return true;
+          return true
         } else {
-          return 'Please enter a valid account name of a Github user.';
+          return 'Please enter a valid account name of a Github user.'
         }
       }
     }
-  ];
+  ]
 
-  inquirer.prompt(questions).then(callback);
+  inquirer.prompt(questions).then(callback)
 }
 
-// Make a GET request to Github API and get all user's repos
 function getRepos(input) {
-  var urlToFetch = util.format(URL_GIT_REPOS, input.username);
-  console.log('>>> url: ', urlToFetch);
-
-
+  var urlToFetch = util.format(URL_GIT_REPOS, input.username)
   var options = {
     url: urlToFetch,
     headers: {
       'User-Agent': 'Gitlang-App'
     }
-  };
+  }
 
   request(options, function (error, response, body) {
     if (!error && response.statusCode === 200) {
-      var arrRepos = JSON.parse(body);
-      console.log(arrRepos);
+      outputPreferredLang(getMostFrequentElement(getLanguages(JSON.parse(body))))
+    } else {
+      console.log('Request failed. Try again.')
     }
-  });
+  })
 }
 
+function getLanguages(arrRepos) {
+  if (arrRepos.length === 0) {
+    return []
+  }
 
-// Iterate thgrough the repos and get the language property
+  arrRepos.forEach(function (repo) {
+    arrLangs.push(repo.language)
+  })
+  return arrLangs
+}
 
+function getMostFrequentElement(arrLangs) {
+  if (arrLangs.length === 0) {
+    return null
+  }
 
-// Implement some logic to verify which is the most used language and return it as output to the user's console
+  var modeMap = {}
+  var maxEl = arrLangs[0]
+  var maxCount = 1
+
+  for (var i = 0; i < arrLangs.length; i++) {
+    var el = arrLangs[i]
+
+    if (modeMap[el] === null) {
+      modeMap[el] = 1
+    } else {
+      modeMap[el]++
+    }
+
+    if (modeMap[el] > maxCount) {
+      maxEl = el
+      maxCount = modeMap[el]
+    }
+  }
+  return maxEl;
+}
+
+function outputPreferredLang(lang) {
+  if (lang === null) {
+    console.log(chalk.red('No such user. Please enter another username.'))
+    return null
+  }
+  console.log(chalk.blue(lang))
+}
